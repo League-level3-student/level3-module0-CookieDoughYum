f0b0f25269ddfd7febd18427233e45b446274ccd
root <root@a9db151f118f>
2020-03-16 23:56:30 +0000

    Working on firework display

--- src/_02_Firework_Display/Firework.java
+++ src/_02_Firework_Display/Firework.java
@@ -1,5 +1,6 @@
 package _02_Firework_Display;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
 
@@ -14,22 +15,28 @@ public class Firework{
 	public static final int GRAVITY = 1;
 	
 	//1. Create an array of Spark objects called sparks. Don't initialize it.
-	
+	Spark[] sparks;
 	public boolean dead = false;
 	
 	public Firework() {
 		//2. Initialize the Spark array to contain 100 Sparks. 
-
+sparks.equals(100);
 		
 		//3. Iterate through the sparks and initialize each one to a new Spark.
 		//   Make each spark start at the middle bottom of the screen.
-	
+	for(int i=0; i<sparks.length; i++) {
+		sparks[i]=new Spark(250, 400);
+	}
 	}
 	
 	public void launch() {
 		//4. Iterate through the sparks and reset their x and y location
 		//   to their original starting point.
-		
+		for(int i=0; i<sparks.length; i++) {
+			sparks[i].x=250;
+			sparks[i].y=400;
+					
+		}
 	}
 	
 	public void updateSparks() {
@@ -66,6 +73,9 @@ public class Firework{
 	
 	public void drawSparks(Graphics g) {
 		//5. Iterate through each spark
+		for(int i=0; i<sparks.length; i++) {
+			sparks[i].setColor(Color.blue);
+		}
 			//6. Set the color to the spark's color with g.setColor()
 			//7. Fill in an oval using the spark's x, y, and size with g.fillOval()
 	}
